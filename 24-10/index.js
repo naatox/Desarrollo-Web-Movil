c94071b77ff334cdfdf50657dacf28ea967d13c3
Renato Morales <104334139+naatox@users.noreply.github.com>
2023-10-24 17:35:32 -0300

    24-10

---
+++ 24-10/index.js
@@ -0,0 +1,14 @@
+const url = "https://jsonplaceholder.typicode.com/users"
+
+const consultarAPI = async () => {
+    const respuesta = await fetch(url)//await previene que se ejecutre la sig linea bloqueando el codigo.
+    console.log(respuesta)
+   
+    const resultado = await respuesta.json()
+    
+        
+    
+    console.log(resultado)
+}
+consultarAPI();
+      
